34ef636493ba728230246be5bccb7f79e4e789c3
Mayank Nagar <nagar.mayank@gmail.com>
2019-09-04 10:05:45 +0530

    Add files via upload

--- IntegerMultiplicationSubQuadratic.py
+++ IntegerMultiplicationSubQuadratic.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import math
 
 def zeroPad(numberString, zeros, left = True):
@@ -30,16 +28,16 @@ def RecursiveMultiplication(A, B):
     split_position = math.ceil(max_len/2)
 
     #Split numbers in high and low order of digits for both the numbers
-    A1 = (A[:-split_position]) 
-    A2 = (A[-split_position:])
-    B1 = (B[:-split_position])
-    B2 = (B[-split_position:])
+    A1 = A[:-split_position] 
+    A2 = A[-split_position:]
+    B1 = B[:-split_position]
+    B2 = B[-split_position:]
     
     f.write("\n--------------------------------")
     f.write("\nIntermediate Values of A1, B1 after partition:")
     f.write("\n--------------------------------")
-    f.write("\nA : "+ A + "  A1 : "+ A1 + " A2 : " + A2) # A:223245 A1: 223 A2: 245
-    f.write("\nB : "+ B + "  B1 : "+ B1 + " B2 : " + B2) # B:123456 B1: 123 B2: 456
+    f.write("\nA : "+ A + "  A1 : "+ A1 + " A2 : " + A2)
+    f.write("\nB : "+ B + "  B1 : "+ B1 + " B2 : " + B2)
     
     #Steps for sub quadratic calculation with 3 multiplications
     A1_B1 = RecursiveMultiplication(A1, B1)
@@ -52,32 +50,33 @@ def RecursiveMultiplication(A, B):
 if __name__ == '__main__':
     filename = 'inputPS2.txt'
     x, y = "", ""
+    num_list = []
     outfile = 'outputPS2.txt'
     f = open(outfile, 'w')
 
     with open (filename, 'r') as infile:
-       for line in infile:
-           if line.startswith("1st"):
-               idx = line.find(":")
-               if idx != -1:
-                   # First Number is present
-                   x = (line[idx+1:]).strip()
-                   if  not x.isdigit():
-                       f.write("\nNot a valid number")
-                       x = ""
-           if line.startswith("2nd"):
-               idx = line.find(":")
-               if idx != -1:
-                   # Second Number is present
-                   y = (line[idx+1:]).strip()
-                   if  not y.isdigit():
-                       f.write("\nNot a valid number")
-                       y = ""
+        for line in infile:
+            if line.strip():
+                num = line.strip()
+                if not num.isdigit():
+                    f.write("\nNot a valid number")
+                    num = ""
+                else:
+                    num_list.append(num)
+    
+    for i, number in enumerate(num_list):
+        if i == 0:
+            x = number
+        if i == 1:
+            y = number
+            
     if len(x) > 1 and len(y) > 1:
         f.write("--------------------------------")
+        f.write("\nFirst Number is: " + x)
+        f.write("\nSecond Number is: " + y)
         f.write("\nResult: > " + x + "  x " + y + "  = " + str(RecursiveMultiplication(x, y)))
         f.write("\n--------------------------------")
-        f.write("\n x*y  directly " + str(int(x) * int(y)))
+
     else:
         f.write("\nEither x or y is invalid")
     f.close()
